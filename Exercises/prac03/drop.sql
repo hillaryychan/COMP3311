cbefddb1cc92aab0f5c704b42fdad56c2b5ffe2e
Hillary Chan <hillary.chan91@gmail.com>
2020-03-11 23:16:18 +1100

    Moved prac and assignments to separate folder from exercises

--- Exercises/prac03/drop.sql
+++
@@ -1,8 +0,0 @@
--- COMP3311 Prac 03 Exercise
--- Remove all tables in the company schema
--- The order of the statements is important
-
-drop table WorksFor;
-drop table DeptMissions;
-drop table Departments;
-drop table Employees;
